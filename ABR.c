a14245fd331f7ecaf613ad37cc8e496a1f0e4af8
TARO958 <>
2023-01-12 11:34:48 +0100

    abr.c les bases

--- ABR.c
+++ ABR.c
@@ -1,11 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h> 
-#include "ABR.h"
+#include "arbre.h"
 
-int source(){
+int main(){
     printf("je suis le boss ");
     return 0;
 }
+
+
+
+//fonction permetttant d'insérer un élément dans l'arbre, et qui renvoie l'arbre
+ pNode insert_élément( )
+
+//fonction de recherche qui prend en entrée une valeur et renvoie un bool en indiquant si la valeur existe dans l'arbre
+bool research_data()
+
+
 
 
 
