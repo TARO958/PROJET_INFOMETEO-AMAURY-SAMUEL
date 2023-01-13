ee99db51ed795e52fc4fd7dad67b30da3933c060
Amaury-75 <82617886+Amaury-75@users.noreply.github.com>
2023-01-13 19:48:52 +0100

    Add files via upload
    modification du Shell nouvautés :
    - obligation de mettre un des 5 argument necessaire (plus mode si besoin)
    - précise quelle est la raison de l'erreur

--- Main.sh
+++ Main.sh
@@ -1,24 +1,92 @@
 #!/bin/bash
 
-letter=$1
+arg1=$1
+Arg_Obligatoire=false
 
-if [ -z "$letter" ] 
+if [ -z "$arg1" ] 
 then 
-    echo "erreur pas de lettre"
+    echo "erreur pas d'argument obligatoire"
     exit 1
 fi
 
-case "$letter" in 
-    "t") echo "lettre T"
-    ;;
-    "p") echo "lettre P"
-    ;;
-    "w") echo "lettre W"
-    ;;
-    "m") echo "lettre M"
-    ;;
-    "h") echo "lettre H"
-    ;;
-    *) echo "lettre autre"
-    ;;
-esac
+echo "Argument inscrit : $*"
+
+
+for arg in "$@"
+do
+
+    case "$arg" in
+        "-t") echo "Il manque le mode à l'argument -t"
+              exit 1
+        ;; 
+        "-t1") Arg_Obligatoire=true 
+             echo "VALIDE"
+             break
+        ;;
+        "-t2") Arg_Obligatoire=true 
+             echo "VALIDE"
+             break
+        ;;
+        "-t3") Arg_Obligatoire=true 
+             echo "VALIDE"
+             break
+        ;;
+        "-p") echo "Il manque le mode à l'argument -p"
+              exit 1
+        ;;
+        "-p1") Arg_Obligatoire=true
+             echo "VALIDE"
+             break
+        ;;
+        "-p2") Arg_Obligatoire=true
+             echo "VALIDE"
+             break
+        ;;
+        "-p3") Arg_Obligatoire=true
+             echo "VALIDE"
+             break
+        ;;
+        "-w") Arg_Obligatoire=true
+             echo "VALIDE"
+             break
+        ;;
+        "-m") Arg_Obligatoire=true
+             echo "VALIDE"
+             break
+        ;;
+        "-h") Arg_Obligatoire=true
+             echo "VALIDE"
+             break
+        ;;
+        *) echo "Erreur l'argument $arg n'existe pas"
+           break
+        ;;  
+    esac
+
+done
+
+# Verification argument 
+if [ $Arg_Obligatoire == true ]
+then 
+    echo "Bool true"
+else 
+    exit 1
+fi
+
+echo "toujours là"
+
+################ BROUILLON ####################
+
+
+# [ $# -ge i ]; vérifie si l'argument "i" existe par comparaison des nombre d'arguments
+
+
+# "t") cut -d';' -f11 meteo_filtered_data_v1.csv # mode 1, 2, 3
+
+# "p") cut -d';' -f3 meteo_filtered_data_v1.csv # mode 1, 2, 3
+            
+# "w") cut -d';' -f4 meteo_filtered_data_v1.csv
+            
+# "m") cut -d';' -f6 meteo_filtered_data_v1.csv # décroissant
+            
+# "h") cut -d';' -f14 meteo_filtered_data_v1.csv # décroissant
